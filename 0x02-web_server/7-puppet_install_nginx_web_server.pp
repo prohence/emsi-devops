ce88427501871db377b8d13a7434d01cb5761b0c
Souhail KARAM <souhail.karam@outlook.com>
2021-05-06 23:24:29 +0000

    Commit 1 - 2

--- 0x02-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x02-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,4 @@
 # Puppet manifest file to install nginx
-
 package { 'nginx':
   ensure => installed,
 }
@@ -8,11 +7,14 @@ file { '/var/www/html/index.html':
   content => 'Holberton School',
 }
 
+file { '/var/www/html/404.html':
+  content => 'Ceci n\'est pas une page',
+}
+
 file_line { 'Add redirection, 301':
-  ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
+  ensure => 'present',
   after  => 'listen 80 default_server;',
-  line   => 'listen [::]:80 default_server;',
   line   => 'rewrite ^/redirect_me http://github.com/prohence permanent;',
 }
 
